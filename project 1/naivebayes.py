dbd1e24e7a4ce7e4e5968539be9fc8f6007bdc2d
raymond-chii <raymondchi56@gmail.com>
2024-02-19 23:55:24 -0500

    update on tokenization

--- project 1/naivebayes.py
+++ project 1/naivebayes.py
@@ -1,17 +1,32 @@
 import os
+from nltk.tokenize import word_tokenize
 
-def read_files_in_folder(folder_path):
-    lines = []
-    for file_name in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file_name)
-        if os.path.isfile(file_path):
-            with open(file_path, 'r') as file:
-                lines.append(file.read().split())
-    return lines
 
-folder_path = 'TC_provided/corpus1/train'
-if not os.path.exists(folder_path):
-    print(f"Folder '{folder_path}' does not exist.")
+def read_file(folder):
+    files_content = {}
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        with open(file_path, 'r') as file:
+            files_content[filename] = file.read()
+    return files_content
+
+def tokenize_file(file_content):
+    tokens = {}
+    for filename, file_content in file_content.items():
+        tokens[filename] = word_tokenize(file_content)
+    return tokens
+
+
+train_folder = 'project 1/TC_provided/corpus1/train'
+
+if not os.path.exists(train_folder):
+    print(f"Folder '{train_folder}' does not exist.")
 else:
-    files_content = read_files_in_folder(folder_path)
-    print(f"Read {len(files_content)} files from folder '{folder_path}'.")
+    files_content = read_file(train_folder)
+    tokens_content = tokenize_file(files_content)
+    print(tokens_content)
+    print(f"Read {len(files_content)} files from folder '{train_folder}'.")
+
+
+
+
